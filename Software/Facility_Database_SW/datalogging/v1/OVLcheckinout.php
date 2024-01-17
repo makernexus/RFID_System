dee85892d36a7c045ba8041fffc8e658812e4b00
Jim Schrempp <github1@jimschrempp.com>
2024-01-16 17:19:41 -0800

    form is working

--- Software/Facility_Database_SW/datalogging/v1/OVLcheckinout.php
+++ Software/Facility_Database_SW/datalogging/v1/OVLcheckinout.php
@@ -34,15 +34,9 @@ if (mysqli_connect_errno()) {
     logfile("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 
-// parse http variables into php variables
-
-$previousVisitNum = 0;
-if (isset($_GET["vid"])) {
-    $previousVisitNum = cleanInput($_GET["vid"]);
-} 
-if (isset($_POST["previousVisitNum"])) {
-    $previousVisitNum = cleanInput($_POST["previousVisitNum"]);
-} 
+$today = new DateTime();  
+$today->setTimeZone(new DateTimeZone("America/Los_Angeles"));
+$nowSQL = $today->format("Y-m-d H:i:s");
 
 // posted data
 $nameFirst =  cleanInput($_POST["nameFirst"]);
@@ -53,6 +47,7 @@ $hasSignedWaiver = cleanInput($_POST["hasSignedWaiver"]);
 $visitReason = "";
 if (isset($_POST["visitReason"])) {
     $visitReason = cleanInput($_POST["visitReason"]);
+    echo "visitReason: " . $visitReason . " has been received.";
 }
 
 $email = "";
@@ -65,25 +60,27 @@ if (isset($_POST["phone"])) {
     $phone = cleanInput($_POST["phone"]);
 }
 
-$today = new DateTime(); 
-$today->setTimeZone(new DateTimeZone("America/Los_Angeles"));
-
-
-# if vid is not set, then exit
-if (!isset($previousVisitNum)) {
-    echo "No visitID received. Exiting.";
-    logfile("No visitID received. Exiting.");
+// previousVisitNum 
+//    -1: post data came from the form, human input
+//     0: request came from a URL with no previous visit
+//   num: data came from a QR code with the previous visit number in it
+$previousVisitNum = 0;
+if (isset($_GET["vid"])) {
+    // this would come from a QR code
+    $previousVisitNum = cleanInput($_GET["vid"]);
+} 
+if (isset($_POST["previousVisitNum"])) {
+    // this would come from the form
+    $previousVisitNum = cleanInput($_POST["previousVisitNum"]);
+} 
+#if vid is not a number, then exit
+if (!is_numeric($previousVisitNum)) {
+    echo "visitID: " . $previousVisitNum . " is not a number. Exiting.";
+    logfile("visitID: " . $previousVisitNum . " is not a number. Exiting.");
     exit;
-} else {
-    #if vid is not a number, then exit
-    if (!is_numeric($previousVisitNum)) {
-        echo "visitID: " . $previousVisitNum . " is not a number. Exiting.";
-        logfile("visitID: " . $previousVisitNum . " is not a number. Exiting.");
-        exit;
-    } else  {
-        echo "visitID: " . $previousVisitNum . " has been received. Thank you.";
-        logfile("visitID: " . $previousVisitNum . " has been received.");
-    }
+} else  {
+    echo "visitID: " . $previousVisitNum . " has been received. Thank you.";
+    logfile("visitID: " . $previousVisitNum . " has been received.");
 }
 
 switch ($previousVisitNum) {
@@ -91,11 +88,12 @@ switch ($previousVisitNum) {
         // if visitid =-1, then this came from the form, add a record to the database
         echo "insert new";
         // insert the new visit into the database
-        insertNewVisitInDatabase($con, $nameFirst, $nameLast, $email, $phone, $visitReason);
+        $previousVisitNum = 0;
+        insertNewVisitInDatabase($con, $nowSQL, $nameFirst, $nameLast, $email, $phone, $visitReason, $previousVisitNum);
         break;
 
     case 0:
-        // Serve up the HTML form
+        // request from a bare URL, serve up the HTML form
         $html = file_get_contents("OVLcheckinout.html");
         if (!$html){
             logfile("unable to open file");
@@ -103,30 +101,22 @@ switch ($previousVisitNum) {
         }
         // send the HTML
         echo $html;
-
         break;
 
     default:
-        // we have a visitID so this is either a checkout, or a new checkin from a repeat visitor
+        // we have a previousVisitNum so this is either a checkout, or a new checkin from a repeat visitor
         echo "previousVisitNum: " . $previousVisitNum . " has been received.";
-        // is the visitID in the database from since the start of the day?
-        $sql = "SELECT * FROM ovl_visits WHERE visitID = " . $visitID 
-            . " AND dateCreated > '" . $today->format("Y-m-d") . "'";
-        $result = mysqli_query($con, $sql);
-        if (!$result) {
-            echo "Error: " . $sql . "<br>" . mysqli_error($con);
-            logfile("Error: " . $sql . "<br>" . mysqli_error($con));
-            exit;
-        } 
+        // is the visitID in the database from since the start of the day without a checkout?
+        $currentCheckInRecNum = getCurrentCheckin($con, $previousVisitNum);
         // if no records in result, then this is a new checkin
-        if (mysqli_num_rows($result) == 0) {
+        if ($currentCheckInRecNum == 0) {
             // this is a new checkin
             // insert the new visit into the database
-            insertNewVisitInDatabase($con, $nameFirst, $nameLast, $email, $phone, $visitReason);
-            echo "insert new with previousVisitNum: " . $previousVisitNum . ".";
+            insertNewVisitInDatabase($con, $nowSQL, $nameFirst, $nameLast, $email, $phone, $visitReason, $previousVisitNum);
+            echo "insert new with previousVisitNum: " . $currentCheckInRecNum . ".";
         } else {
             // this is a checkout
-            updateVisitInDatabase($con, $result["recNum"]);
+            updateVisitInDatabase($con, $nowSQL, $currentCheckInRecNum);
             echo "checkout";
         }
 
@@ -137,13 +127,20 @@ switch ($previousVisitNum) {
         exit;
 }
 
-function insertNewVisitInDatabase($con, $nameFirst, $nameLast, $email, $phone, $visitReason) {
-    $today = new DateTime();
+function insertNewVisitInDatabase($con, $nowSQL, $nameFirst, $nameLast, $email, $phone, $visitReason, $previousVisitNum) {
 
-    $sql = "INSERT INTO ovl_visits SET nameFirst = '" . $nameFirst . "', nameLast = '" . $nameLast . "'," 
-        . " email = '" . $email . "', phone = '" . $phone . "'," 
-        . " visitReason = '" . $visitReason . "'"
-        . ", timeCheckinLocal = '" . $today->format("Y-m-d H:i:s") . "'";
+
+    $sql = "INSERT INTO ovl_visits SET"
+        . " nameFirst = '" . $nameFirst . "',"
+        . " nameLast = '" . $nameLast . "'," 
+        . " email = '" . $email . "', "
+        . " phone = '" . $phone . "'," 
+        . " visitReason = '" . $visitReason . "',"
+        . " previousRecNum = " . $previousVisitNum . ","
+        . " dateCreatedLocal = '" . $nowSQL . "',"
+        . " dateCheckinLocal = '" . $nowSQL  . "'";
+
+    echo "sql: " . $sql;
 
     $result = mysqli_query($con, $sql);
     if (!$result) {
@@ -156,12 +153,39 @@ function insertNewVisitInDatabase($con, $nameFirst, $nameLast, $email, $phone, $
     }
 }
 
-function updateVisitInDatabase($con, $visitID) {
+function updateVisitInDatabase($con, $nowSQL, $visitID) {
 
-    $today = new DateTime();
-    $sql = "UPDATE ovl_visits SET dateOut = '" . $today->format("Y-m-d H:i:s") 
-        . " timeCheckoutLocal = '" . $today->format("Y-m-d H:i:s")
-        . "' WHERE visitID = " . $visitID;
+    $sql = "SELECT dateCheckinLocal FROM ovl_visits " 
+        . " WHERE recNum = " . $visitID;
+    $result = mysqli_query($con, $sql);
+    if (!$result) {
+        echo "Error: " . $sql . "<br>" . mysqli_error($con);
+        logfile("Error: " . $sql . "<br>" . mysqli_error($con));
+        exit;
+    } else {
+        $row = mysqli_fetch_assoc($result);
+        $dateCheckinLocal = $row["dateCheckinLocal"];
+    }
+
+    // calculate elapsed time
+    $dateCheckinLocal = new DateTime($dateCheckinLocal);
+    $dateCheckinLocal->setTimeZone(new DateTimeZone("America/Los_Angeles"));
+    $dateCheckoutLocal = new DateTime($nowSQL);
+    $dateCheckoutLocal->setTimeZone(new DateTimeZone("America/Los_Angeles"));
+    $interval = $dateCheckinLocal->diff($dateCheckoutLocal);
+    $elapsedTime = $interval->format('%h');
+
+
+    $sql = "UPDATE ovl_visits SET "
+        . " dateUpdated = '" . $nowSQL . "',"
+        . " dateUpdatedLocal = '" . $nowSQL . "',"
+        . " dateCheckoutLocal = '" . $nowSQL . "',"
+        . " elapsedHours = " . $elapsedTime . ","
+        . " labelNeedsPrinting = 0"
+        . " WHERE recNum = " . $visitID;
+
+    echo "sql: " . $sql;
+
     $result = mysqli_query($con, $sql);
     if (!$result) {
         logfile("Error: " . $sql . "<br>" . mysqli_error($con));
@@ -171,6 +195,34 @@ function updateVisitInDatabase($con, $visitID) {
         echo "Record updated successfully";
         logfile("Record updated successfully");
     }
+}
+
+function getCurrentCheckin ($con, $visitID){
+    $today = new DateTime();
+    $today->setTimeZone(new DateTimeZone("America/Los_Angeles"));
+    $todaySQL = $today->format("Y-m-d"); // date only, no time
+
+    $sql = "SELECT max(recNum) as recNum FROM ovl_visits WHERE "
+            . "(recNum = " . $visitID . " OR previousRecNum = " . $visitID . ")"
+            . " AND dateCreated > '" . $todaySQL . "'"
+            . " AND dateCheckoutLocal = '0000-00-00 00:00:00'";
+
+    echo "sql: " . $sql;
+
+    $result = mysqli_query($con, $sql);
+    if (!$result) {
+        echo "Error: " . $sql . "<br>" . mysqli_error($con);
+        logfile("Error: " . $sql . "<br>" . mysqli_error($con));
+        exit;
+    } 
+    # if results are empty, then there is no open checkin from today
+    if (mysqli_num_rows($result) == 0) {
+        $currentCheckInRecNum = 0;
+    } else {
+        $row = mysqli_fetch_assoc($result);
+        $currentCheckInRecNum = $row["recNum"];
+    }
+    return $currentCheckInRecNum;
 }
 
 function logfile($logEntry) {
